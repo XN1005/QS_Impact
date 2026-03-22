c159820b968264ba459f2a9831ee83ab3450e059
Tien Dat <datnguyentien1303@gmail.com>
2026-03-22 15:17:49 +0700

    file organization

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,9 +1,7 @@
 extends Control
 
-
-
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://MAIN_SCENE.tscn")
+	get_tree().change_scene_to_file("res://scenes/MAIN_SCENE.tscn")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
